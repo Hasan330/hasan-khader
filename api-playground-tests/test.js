a9e601dbc1d8ba47948399fb75039fa22cbe4aad
Hasan Khader <hasan.saleh.khader@gmail.com>
2017-12-09 03:50:05 +0200

    Setting up test cases

--- api-playground-tests/test.js
+++ api-playground-tests/test.js
@@ -1,8 +1,46 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
+const assert = require('assert');
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const chaiJsonSchema = require('chai-json-schema');
+const chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiJsonSchema);
+chai.use(chaiAsPromised);
+chai.use(chaiHttp);
+
+
+const expect = chai.expect;
+const request = chai.request
+
+chai.tv4.banUnknown = true;
+chai.tv4.multiple = false;
+chai.tv4.cyclicCheck = false;
+
+
+describe('General Failure Cases', function(){
+	it('handles searching for wrong route', async function(){
+		try{
+			let wrongURL = await request('localhost:3030')
+        	.get(`/123`)
+		}
+		catch(err){
+			console.log(err.response.body)
+			expect(err).to.have.status(404);
+		}        
+	})
+})
+
+
+
+describe('Products API', function () {
+  it('can get product by id', async function () {
+  	let sampleProductID = 43900
+  	
+  	const product = await request('localhost:3030')
+        .get(`/products/${sampleProductID}`)
+  	
+
+    console.log(product.body)
+    expect(product).to.have.status(200)
   });
-});
+})
